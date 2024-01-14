5124d8a0ca3ab2fa184e7c7c7884308b86fd9ad3
Carol Jung <cjung@roblox.com>
2024-01-13 17:19:51 -0800

    Chain response with reflection

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI, HTTPException, Form
+PRIMER = "You are an efficient and concise programming assistant that only responds in code."
+GENERATE_DIFF = "Write a unified github diff that applies the following changes to {}:\n{}"
+REFLECT = "Can you generate an even more accurate, efficient, and concise diff? If not, return 'stop'"
+
+MAX_ITERATIONS = 5
+STOP_TOKEN = 'stop'
+
+from enum import Enum
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from openai import OpenAI
 
 app = FastAPI()
 client = OpenAI()
+messages = []
 
-text_diff_prompt = "Generate a unified github diff that applies the following changes to {}:\n{}"
+class Role(str, Enum):
+    SYSTEM = 'system'
+    USER = 'user'
+    ASSISTANT = 'assistant'
 
 class RequestModel(BaseModel):
     repoUrl: str
@@ -13,27 +25,50 @@ class RequestModel(BaseModel):
 
 class ResponseModel(BaseModel):
     unified_diff: str
+    
+def reset_messages():
+    messages.clear()
+    messages.append({"role": Role.SYSTEM, "content": PRIMER})
 
-async def get_unified_diff(repo_url, prompt, max_tokens=200, timeout=30):
-    # Use OpenAI API to apply the prompt to the unified diff
+async def prompt_gpt():
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-                {"role": "system", "content": text_diff_prompt.format(repo_url, prompt)}
-        ]
+        messages=messages
     )
+    content = response.choices[0].message.content
+    messages.append({"role": Role.ASSISTANT, "content": content})
+    print(content)
+    return content
 
-    return response.choices[0].message.content
+async def get_unified_diff(repo_url, prompt):
+    messages.append({"role": Role.USER, "content": GENERATE_DIFF.format(repo_url, prompt)})
+    return await prompt_gpt()
 
+async def reflect():
+    messages.append({"role": Role.USER, "content": REFLECT})
+    return await prompt_gpt()
+    
 
 @app.post("/generate-diff/", response_model=ResponseModel)
 async def generate_diff(request_data: RequestModel):
     repo_url = request_data.repoUrl
     prompt = request_data.prompt
 
+    reset_messages()
+
     try:
         unified_diff = await get_unified_diff(repo_url, prompt)
-        return {"unified_diff": unified_diff}
+        
+        for i in range(MAX_ITERATIONS):
+            reflection = await reflect()
+            if reflection.lower() == STOP_TOKEN:
+                break
+            else:
+                unified_diff = reflection
+            
+        return {
+            "unified_diff": unified_diff,
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
